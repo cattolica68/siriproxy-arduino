05781f16b9052939b2d49390f9573486be7ad0b8
Shawn C <mongo527@gmail.com>
2012-09-25 16:49:43 -0400

    x10 Commands

--- lib/siriproxy-arduino.rb
+++ lib/siriproxy-arduino.rb
@@ -5,8 +5,8 @@ require 'json'
 require 'net/http'
 
 #############
-# This is a plugin for SiriProxy that will allow you to control SickBeard.
-# Example usage: "search my shows backlog."
+# This is a plugin for SiriProxy that will allow you to control x10 Devices with Arduino.
+# Example usage: "turn on living room lights"
 #############
 
 class SiriProxy::Plugin::Arduino < SiriProxy::Plugin
@@ -16,10 +16,75 @@ class SiriProxy::Plugin::Arduino < SiriProxy::Plugin
         @port = config["arduino_port"]
     end
     
-    listen_for /hello (arduino|circuit board)/i do
+    listen_for /hello (arduino|circuit board)/i do |response|
         begin
-            say "This is Mongo's Arduino speaking!"
+            if(response.downcase.include? "dining room")
+        		if(response.downcase.include? "lights")
+            		server = arduinoParser("DRLights", "ON")
+            	end
+            
+            elsif(response.downcase.include? "office")
+        		if(response.downcase.include? "fan")
+        			server = arduinoParser("OfficeFan", "ON")
+        		end
+        	end
+            
+            if(server.code == 200)
+            	say "The #{response} are now on!"
+            
+            else
+            	say "Sorry there was a problem"
+            end 
+            	
+        rescue Errno::EHOSTUNREACH
+            say "Sorry, I could not connect to your Arduino."
+        rescue Errno::ECONNREFUSED
+            say "Sorry, the Arduino is not running."
+        rescue Errno::ENETUNREACH
+            say "Sorry, Could not connect to the network."
+        rescue Errno::ETIMEDOUT
+            say "Sorry, The operation timed out."
         end
         request_completed
+    end
+    
+    listen_for /(turn off (.*) | turn (.*) off)/i do |response|
+        begin
+        	if(response.downcase.include? "dining room")
+        		if(response.downcase.include? "lights")
+        			server = arduinoParser("DRLights", "OFF")
+        		end
+        	
+        	elsif(response.downcase.include? "office")
+        		if(response.downcase.include? "fan")
+        			server = arduinoParser("OfficeFan", "OFF")
+        		end
+        	end
+        	
+        	if(server.code == 200)
+            	say "The #{response} are now on!"
+            
+            else
+            	say "Sorry there was a problem"
+            end 
+            	
+        rescue Errno::EHOSTUNREACH
+            say "Sorry, I could not connect to your Arduino."
+        rescue Errno::ECONNREFUSED
+            say "Sorry, the Arduino is not running."
+        rescue Errno::ENETUNREACH
+            say "Sorry, Could not connect to the network."
+        rescue Errno::ETIMEDOUT
+            say "Sorry, The operation timed out."
+        end
+		request_completed
+	end
+
+    def arduinoParser(dvc, cmd)
+        
+        url = "http://#{@host}:#{@port}/?device=#{dvc}&cmd=#{cmd}"
+        resp = Net::HTTP.get_response(URI.parse(url))
+        
+        return resp;
     end
 end
