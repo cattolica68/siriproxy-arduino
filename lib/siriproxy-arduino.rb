d0ac9eb4badd6259ad4cd1c8d39546309ade7ede
Shawn C <mongo527@gmail.com>
2013-01-13 21:36:32 -0500

    Cleaned Hash

--- lib/siriproxy-arduino.rb
+++ lib/siriproxy-arduino.rb
@@ -16,19 +16,19 @@ class SiriProxy::Plugin::Arduino < SiriProxy::Plugin
         @port = config["arduino_port"]
     end
     
-    x10 = Hash['dining room lights', "DRLights", 'dining room light', "DRLights", 'front porch lights', "FrontPorchLights", 'front porch light', "FrontPorchLights"]
+    x10 = Hash['dining room lights', "DRLights", 'front porch lights', "FrontPorchLights"]
     
     listen_for /((turn (on|on the|off|off the) (.+))|(turn the (.+) (on|off)))/i do |response|
         begin
             x10.each_key do |loc|
                 if response.downcase.include? loc
                     if response.downcase.include? "on"
-                        server = arduinoParser(x10[response.downcase], "ON")
+                        server = arduinoParser(x10[loc], "ON")
                     elsif response.downcase.include? "off"
-                        server = arduinoParser(x10[response.downcase], "OFF")
+                        server = arduinoParser(x10[loc], "OFF")
                     end
                     if server.code == "200"
-                        say "The #{response} are now ON!"
+                        say "The #{loc} are now ON!"
                     end
                 end
             end
